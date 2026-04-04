04b3e1d683cd196a6061b977246ed102112f3467
Sergey Podolskiy <sergeypodolskiy00@gmail.com>
2026-04-04 21:53:43 +0300

    Updated logo.dev API with valid publishable key

--- src/components/Slide15Waitlist.jsx
+++ src/components/Slide15Waitlist.jsx
@@ -46,7 +46,7 @@ const CompanyLogo = ({ company }) => {
       padding: '0.25rem'
     }}>
       <img 
-        src={`https://img.logo.dev/${company.domain}?token=sk_MgypntRdRD2WdWoL4jbdGA`} 
+        src={`https://img.logo.dev/${company.domain}?token=pk_Fvbs8Zl6SWiC5WEoP8Qzbg`} 
         alt={company.name}
         title={company.name}
         style={{ 
